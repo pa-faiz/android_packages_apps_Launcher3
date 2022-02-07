09d4a5c71c482a68212cb4f7f715187917db3e58
LibXZR <i@xzr.moe>
2022-02-07 10:59:50 +0800

    Launcher3: Introduce memory info in overview
    This is convenient for monitoring device status and many OEMs have their design for this. It's time to have our own one.
    
    Change-Id: I890c86c798055b1964506d9a4df1cef386562160
    Signed-off-by: LibXZR <i@xzr.moe>
    iusmac: adapt to A13.
    Signed-off-by: iusmac <iusico.maxim@libero.it>
    neobuddy89: adapt to A13 QPR2
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Alvin Francis <nivlafx@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    
    Update aospa_strings.xml

--- quickstep/src/com/android/launcher3/uioverrides/states/OverviewModalTaskState.java
+++ quickstep/src/com/android/launcher3/uioverrides/states/OverviewModalTaskState.java
@@ -48,7 +48,7 @@ public class OverviewModalTaskState extends OverviewState {
     @Override
     public int getVisibleElements(Launcher launcher) {
         boolean clearAll = LauncherPrefs.getPrefs(launcher).getBoolean("pref_recents_clear_all", true);
-        return OVERVIEW_ACTIONS | (!clearAll ? CLEAR_ALL_BUTTON : 0);
+        return OVERVIEW_ACTIONS | MEMINFO | (!clearAll ? CLEAR_ALL_BUTTON : 0);
     }
 
     @Override
--- quickstep/src/com/android/quickstep/RecentsActivity.java
+++ quickstep/src/com/android/quickstep/RecentsActivity.java
@@ -68,6 +68,7 @@ import com.android.launcher3.taskbar.FallbackTaskbarUIController;
 import com.android.launcher3.taskbar.TaskbarManager;
 import com.android.launcher3.util.ActivityOptionsWrapper;
 import com.android.launcher3.util.ActivityTracker;
+import com.android.launcher3.util.DisplayController;
 import com.android.launcher3.util.RunnableList;
 import com.android.launcher3.util.SystemUiController;
 import com.android.launcher3.util.Themes;
@@ -80,6 +81,7 @@ import com.android.quickstep.fallback.RecentsState;
 import com.android.quickstep.util.RecentsAtomicAnimationFactory;
 import com.android.quickstep.util.SplitSelectStateController;
 import com.android.quickstep.util.TISBindHelper;
+import com.android.quickstep.views.MemInfoView;
 import com.android.quickstep.views.OverviewActionsView;
 import com.android.quickstep.views.RecentsView;
 import com.android.quickstep.views.TaskView;
@@ -108,6 +110,7 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
     private OverviewActionsView mActionsView;
     private TISBindHelper mTISBindHelper;
     private @Nullable FallbackTaskbarUIController mTaskbarUIController;
+    private MemInfoView mMemInfoView;
 
     private StateManager<RecentsState> mStateManager;
 
@@ -130,13 +133,17 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
         mScrimView = findViewById(R.id.scrim_view);
         mFallbackRecentsView = findViewById(R.id.overview_panel);
         mActionsView = findViewById(R.id.overview_actions_view);
+        mMemInfoView = findViewById(R.id.meminfo);
         getRootView().getSysUiScrim().getSysUIProgress().updateValue(0);
         mSplitSelectStateController =
                 new SplitSelectStateController(this, mHandler, getStateManager(),
                         null /* depthController */, getStatsLogManager(),
                         SystemUiProxy.INSTANCE.get(this), RecentsModel.INSTANCE.get(this));
         mDragLayer.recreateControllers();
-        mFallbackRecentsView.init(mActionsView, mSplitSelectStateController);
+        mFallbackRecentsView.init(mActionsView, mSplitSelectStateController, mMemInfoView);
+
+        mMemInfoView.setDp(mDeviceProfile);
+        mMemInfoView.updateVerticalMargin(DisplayController.getNavigationMode(this));
 
         mTISBindHelper = new TISBindHelper(this, this::onTISConnected);
     }
@@ -215,6 +222,10 @@ public final class RecentsActivity extends StatefulActivity<RecentsState> {
 
     public OverviewActionsView getActionsView() {
         return mActionsView;
+    }
+
+    public MemInfoView getMemInfoView() {
+        return mMemInfoView;
     }
 
     @Override
