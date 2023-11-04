0ebb42237f0d77b2210903cee989477ba419d797
electimon <electimon@gmail.com>
2023-11-04 16:40:52 +0800

    Launcher3: Add lens to landscape tasks list
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: I281959919337d6a09a3ba3a56fb3ba965dc0290e
    Signed-off-by: SahilSonar <sss.sonar2003@gmail.com>
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- quickstep/src/com/android/quickstep/TaskShortcutFactory.java
+++ quickstep/src/com/android/quickstep/TaskShortcutFactory.java
@@ -47,6 +47,7 @@ import com.android.launcher3.BaseDraggingActivity;
 import com.android.launcher3.DeviceProfile;
 import com.android.launcher3.R;
 import com.android.launcher3.config.FeatureFlags;
+import com.android.launcher3.Utilities;
 import com.android.launcher3.logging.StatsLogManager.LauncherEvent;
 import com.android.launcher3.model.WellbeingModel;
 import com.android.launcher3.popup.SystemShortcut;
@@ -415,6 +416,25 @@ public interface TaskShortcutFactory {
             }
 
             return Collections.singletonList(new FloatingSystemShortcut(activity, taskContainer));
+        }
+    };
+
+    TaskShortcutFactory LENS = new TaskShortcutFactory() {
+        @Override
+        public List<SystemShortcut> getShortcuts(BaseDraggingActivity activity,
+                TaskIdAttributeContainer taskContainer) {
+            final Task task  = taskContainer.getTask();
+            if (!task.isDockable) {
+                return null;
+            }
+            if (!Utilities.isGSAEnabled(taskContainer.getTaskView().getContext())) {
+                return null;
+            }
+
+            SystemShortcut lensShortcut = taskContainer.getThumbnailView().getTaskOverlay()
+                    .getLensShortcut(activity, taskContainer.getItemInfo(),
+                            taskContainer.getTaskView());
+            return createSingletonShortcutList(lensShortcut);
         }
     };
 
