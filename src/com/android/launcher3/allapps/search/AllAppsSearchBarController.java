68fd2adf395baca7b778b724ae9462021fe7a2bf
Adithya R <gh0strider.2k18.reborn@gmail.com>
2025-02-27 21:02:24 +0530

    Launcher3: Do not hide keyboard on clearing search query
    If the user hits backspace when there is only one character left,
    search gets reset back into all apps view and they keyboard gets
    hidden. Then the user has to press the search bar again to open the
    keyboard which is poor UX. Instead do not hide the keyboard when the
    search bar is reset. Touching elsewhere on the screen already hides
    the keyboard.
    
    Change-Id: Ia41f803d6ffe258923e90ed6943aa214b193e393

--- src/com/android/launcher3/allapps/search/AllAppsSearchBarController.java
+++ src/com/android/launcher3/allapps/search/AllAppsSearchBarController.java
@@ -155,10 +155,7 @@ public class AllAppsSearchBarController
     public void reset() {
         mCallback.clearSearchResult();
         mInput.reset();
-        mInput.clearFocus();
-        mInput.hideKeyboard();
         mQuery = null;
-        mInput.removeOnFocusChangeListener(this);
     }
 
     /**
